daed90f07106414706e653322310d4886ce82d66
Yandhile <yandhilemaluleke@gmail.com>
2025-09-21 14:11:24 +0200

    feat: refactor main.js to enhance app initialization with Pinia and Vue Query integration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import './style.css'
 import './styles/navbarstyles.css'
+import './styles/apod.css'
+import './styles/apod-calendar.css'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+app.use(router)
+app.use(VueQueryPlugin)
+
+app.mount('#app')
