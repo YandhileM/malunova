b03c967e3896970aa7d737f14572fca86ff4e266
Yandhile <yandhilemaluleke@gmail.com>
2025-09-21 12:53:29 +0200

    feat: add StyleGuide view with StyleGuideComponent integration

---
+++ src/views/StyleGuide.vue
@@ -0,0 +1,15 @@
+<script setup>
+import StyleGuideComponent from '../components/StyleGuide.vue'
+</script>
+
+<template>
+  <div class="style-guide-view">
+    <StyleGuideComponent />
+  </div>
+</template>
+
+<style scoped>
+.style-guide-view {
+  min-height: calc(100vh - 4rem);
+}
+</style>
